4f142dfe2672a35ee11c666680eb542708c088d4
kazarian8 <firelightinitiative@gmail.com>
2026-09-03 19:09:18 -0700

    Rename bottom navigation to Library

--- components/ArtistBottomNav.tsx
+++ components/ArtistBottomNav.tsx
@@ -16,7 +16,7 @@ const items: readonly NavItem[] = [
   { label: "Moments", href: "/moments", icon: Radio },
   { label: "Workstation", href: "/workstation", icon: SlidersHorizontal, center: true },
   { label: "Market", href: "/sound-library", icon: Music2 },
-  { label: "Private Library", href: "/local-library", icon: LibraryBig },
+  { label: "Library", href: "/local-library", icon: LibraryBig },
 ];
 
 export default function ArtistBottomNav() {
